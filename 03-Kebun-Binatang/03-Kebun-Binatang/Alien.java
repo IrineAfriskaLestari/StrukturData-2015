a7e90f6eef2219fa818f1ae3f66d00e80577fb65
IrineAfriskaLestari <irin.iren02@yahoo.com>
2015-11-05 17:42:16 +0700

    Irine.19

---
+++ 03-Kebun-Binatang/03-Kebun-Binatang/Alien.java
@@ -0,0 +1,15 @@
+
+
+public class Alien extends Komodo implements Tumbuhan
+{
+    public String suara(){
+        String suara=new String ("Bip");
+        return suara;
+    }
+    
+    public boolean berbuah(){
+        boolean berbuah = true;
+        return berbuah;
+    }
+   
+}
---
+++ 03-Kebun-Binatang/03-Kebun-Binatang/Binatang.java
@@ -0,0 +1,4 @@
+public interface Binatang
+{
+   public String suara();
+}
