13d208d5fa2541266f871fde9b15d9cfae6b5106
IrineAfriskaLestari <irin.iren02@yahoo.com>
2015-11-06 14:42:02 +0700

    Irine.19

---
+++ 04-Profil-Test-Awal/04-Profil-Test-Awal/Profil.java
@@ -0,0 +1,29 @@
+
+/**
+ *Irine.19
+ */
+public class Profil
+{
+   private String nama="Irine Afriska Lestari";
+   private String nim="1408107010019";
+   
+   public Profil(){
+       
+    }
+    
+     public Profil(String nama,String nim){
+        this.nama=nama;
+        this.nim=nim;
+       
+    }
+    
+    public String getNama(){
+        return nama;
+    }
+    
+    public String getNim(){
+        return nim;
+    }
+   
+   
+}
